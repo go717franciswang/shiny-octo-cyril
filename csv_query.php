9223613e19237c69fd387bd458c250e760aa21e7
francis <go717franciswang@gmail.com>
2013-04-02 19:24:45 -0700

    First commit

---
+++ csv_query.php
@@ -0,0 +1,30 @@
+<?php
+
+# example usage
+$campaign_id = CsvQuery::Field('campaign_id');
+$kwd = CsvQuery::Field('okkw', array(
+    'mappers' => array('TRIM', 'LOWER'), 
+    'alias' => 'kwd'
+));
+$sst_in = CsvQuery::Field('clicks_incoming', array(
+    'reducers' => array('SUM'),
+    'alias' => 'sst_in',
+));
+
+$query = new CsvQuery
+    ->select(array($campaign_id, $kwd, $sst_in))
+    ->from($filename)
+    ->where(array(
+        array($campaign_id, 'IN', $campaign_ids), 
+        'AND',
+        array($kwd, '!=', 'foo'),
+    )))
+    ->group_by(array($campaign_id, $kwd));
+
+$result = $query->execute();
+
+# $result = array_like_object(
+#   row('campaign_id' => .., 'kwd' => .., 'sst_in' => ..),
+#   row('campaign_id' => .., 'kwd' => .., 'sst_in' => ..),
+#   ...
+# );
